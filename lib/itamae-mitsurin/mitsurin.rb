5f28fb5a4106e532a39776cf3059a87a301772d0
root <kamiyama@qualitia.co.jp>
2016-03-14 17:35:45 +0900

    changed resource.rb

--- lib/itamae-mitsurin/mitsurin.rb
+++ lib/itamae-mitsurin/mitsurin.rb
@@ -2,10 +2,6 @@ require 'itamae-mitsurin/version'
 require 'itamae-mitsurin/mitsurin/cli'
 require 'itamae-mitsurin/mitsurin/creators'
 require 'aws-sdk'
-#require 'itamae-mitsurin/mitsurin/itamae_task'
-#require 'itamae-mitsurin/mitsurin/serverspec_task'
-#require 'itamae-mitsurin/mitsurin/itamae_with_git_task'
-
 
 module ItamaeMitsurin
   module Mitsurin
