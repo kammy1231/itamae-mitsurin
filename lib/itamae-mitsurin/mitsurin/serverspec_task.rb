7a20dede0579b3b6f862392ed77bc241e57004b0
root <kamiyama@qualitia.co.jp>
2016-03-14 17:35:18 +0900

    mitsurin tasks require base

--- lib/itamae-mitsurin/mitsurin/serverspec_task.rb
+++ lib/itamae-mitsurin/mitsurin/serverspec_task.rb
@@ -1,123 +1,86 @@
 require 'json'
 require 'highline'
+require 'itamae-mitsurin/mitsurin/task_base'
 include Rake::DSL if defined? Rake::DSL
 
 module Itamae
   module Mitsurin
     class ServerspecTask
 
-      class << self
+      namespace :spec do
+        all = []
+        Dir.glob("tmp-nodes/**/*.json").each do |node_file|
 
-        task :spec => 'spec:all'
-        task :default => :spec
+          file_name = File.basename(node_file, '.json')
+          node_attr = JSON.parse(File.read(node_file), symbolize_names: true)
 
-        def self.get_roles(node_file)
-          roles = []
-          JSON.parse(File.read(node_file))['run_list'].each do |role|
-            roles << role.gsub(/role\[(.+)\]/, '\1') if /role\[(.+)\]/ === role
-          end
-          roles
-        end
+          desc "Spec to #{file_name}"
+          task node_attr[:environments][:hostname].split(".")[0] do
 
-        def self.get_recipes(role)
-          recipes = []
-          JSON.parse(File.read("roles/#{role}.json"))['run_list'].each do |recipe|
-            if /recipe\[(.+)::(.+)\]/ === recipe
-              recipes << {recipe.gsub(/recipe\[(.+)::(.+)\]/, '\1') => recipe.gsub(/recipe\[(.+)::(.+)\]/, '\2')}
-            else
-              recipes << {recipe.gsub(/recipe\[(.+)\]/, '\1') => nil}
+          begin
+            recipes = []
+            TaskBase.get_roles(node_file).each do |role|
+              recipes << TaskBase.get_recipes(role)
             end
-          end
-          recipes
-        end
-
-        def self.get_node_recipes(node_file)
-          recipes = []
-          JSON.parse(File.read(node_file))['run_list'].each do |recipe|
-            if /recipe\[(.+)::(.+)\]/ === recipe
-              recipes << {recipe.gsub(/recipe\[(.+)::(.+)\]/, '\1') => recipe.gsub(/recipe\[(.+)::(.+)\]/, '\2')}
-            else
-              recipes << {recipe.gsub(/recipe\[(.+)\]/, '\1') => nil} unless /role\[(.+)\]/ === recipe
+            TaskBase.get_node_recipes(node_file).each do |recipe|
+              recipes << recipe
             end
+          rescue Exception => e
+            puts e.class.to_s + ", " + e.backtrace[0].to_s
+            puts "nodefile or role error, nodefile:#{node_file} reason:#{e.message}"
+            exit 1
+          else
+            recipes << {'_base' => nil}
+            recipes.flatten!
           end
-          recipes
-        end
 
-        hl = HighLine.new
+          node_name = node_attr[:environments][:hostname]
+          ssh_user = node_attr[:environments][:ssh_user]
+          ssh_password = node_attr[:environments][:ssh_password]
+          sudo_password = node_attr[:environments][:sudo_password]
+          ssh_port = node_attr[:environments][:ssh_port]
+          ssh_key = node_attr[:environments][:ssh_key]
 
-        namespace :spec do
-          all = []
-          Dir.glob("tmp-nodes/**/*.json").each do |node_file|
-
-            file_name = File.basename(node_file, '.json')
-            node_attr = JSON.parse(File.read(node_file), symbolize_names: true)
-
-            desc "Spec to #{file_name}"
-            task node_attr[:environments][:hostname].split(".")[0] do
-
-            begin
-              recipes = []
-              get_roles(node_file).each do |role|
-                recipes << get_recipes(role)
-              end
-              get_node_recipes(node_file).each do |recipe|
-                recipes << recipe
-              end
-            rescue Exception => e
-              puts e.class.to_s + ", " + e.backtrace[0].to_s
-              puts "nodefile or role error, nodefile:#{node_file} reason:#{e.message}"
-              exit 1
-            else
-              recipes << {'_base' => nil}
-              recipes.flatten!
-            end
-
-            node_name = node_attr[:environments][:hostname]
-            ssh_user = node_attr[:environments][:ssh_user]
-            ssh_password = node_attr[:environments][:ssh_password]
-            sudo_password = node_attr[:environments][:sudo_password]
-            ssh_port = node_attr[:environments][:ssh_port]
-            ssh_key = node_attr[:environments][:ssh_key]
-
-            node_short = node_name.split(".")[0]
+          node_short = node_name.split(".")[0]
             all << node_short
 
             desc "Run spec to #{file_name}"
-              ENV['TARGET_HOST'] = node_name
-              ENV['NODE_FILE'] = node_file
-              ENV['SSH_PASSWORD'] = ssh_password
-              ENV['SUDO_PASSWORD'] = sudo_password
-              ENV['SSH_KEY'] = "keys/#{ssh_key}"
-              ENV['SSH_PORT'] = ssh_port
-              ENV['SSH_USER'] = ssh_user
+            ENV['TARGET_HOST'] = node_name
+            ENV['NODE_FILE'] = node_file
+            ENV['SSH_PASSWORD'] = ssh_password
+            ENV['SUDO_PASSWORD'] = sudo_password
+            ENV['SSH_KEY'] = "keys/#{ssh_key}"
+            ENV['SSH_PORT'] = ssh_port
+            ENV['SSH_USER'] = ssh_user
 
-              specs = "bundle exec rspec"
+            specs = "bundle exec rspec"
 
-              # recipe load to_spec
-              spec_pattern = []
-              recipes.each do |spec_h|
-                if spec_h["#{spec_h.keys.join}"].nil?
-                  spec_pattern <<
-                      " #{Dir.glob("site-cookbooks/**/#{spec_h.keys.join}/spec/default_spec.rb").join}"
-                else
-                  spec_pattern <<
-                      " #{Dir.glob("site-cookbooks/**/#{spec_h.keys.join}/spec/#{spec_h["#{spec_h.keys.join}"]}_spec.rb").join}"
-                end
+            # recipe load to_spec
+            spec_pattern = []
+            recipes.each do |spec_h|
+              if spec_h["#{spec_h.keys.join}"].nil?
+                spec_pattern <<
+                    " #{Dir.glob("site-cookbooks/**/#{spec_h.keys.join}/spec/default_spec.rb").join}"
+              else
+                spec_pattern <<
+                    " #{Dir.glob("site-cookbooks/**/#{spec_h.keys.join}/spec/#{spec_h["#{spec_h.keys.join}"]}_spec.rb").join}"
               end
-              spec_pattern.sort_by! {|item| File.dirname(item)}
-              specs << spec_pattern.join
-              run_list_noti = []
-              spec_pattern.each {|c_spec| run_list_noti << c_spec.split("/") [2]}
-              puts hl.color(%!Run Serverspec to \"#{node_name}\"!, :red)
-              puts hl.color(%!Run List to \"#{run_list_noti.uniq.join(", ")}\"!, :green)
-              st = system specs
-              exit 1 unless st
             end
-            task :all => all
-            task :default => :all
+
+            spec_pattern.sort_by! {|item| File.dirname(item)}
+            specs << spec_pattern.join
+            run_list_noti = []
+            spec_pattern.each {|c_spec| run_list_noti << c_spec.split("/") [2]}
+            puts TaskBase.hl.color(%!Run Serverspec to \"#{node_name}\"!, :red)
+            puts TaskBase.hl.color(%!Run List to \"#{run_list_noti.uniq.join(", ")}\"!, :green)
+            st = system specs
+            exit 1 unless st
           end
+        task :all => all
+        task :default => :all
         end
       end
+
     end
   end
 end
---
+++ lib/itamae-mitsurin/mitsurin/task_base.rb
@@ -0,0 +1,72 @@
+
+module ItamaeMitsurin
+  module Mitsurin
+    module TaskBase
+
+      class << self
+        class ::Hash
+          def deep_merge(other)
+            merger = lambda {|key, v1, v2| Hash === v1 && Hash === v2 ? v1.merge(v2, &merger) : v2}
+            self.merge(other, &merger)
+          end
+
+          def deep_merge!(other)
+            merger = lambda {|key, v1, v2| Hash === v1 && Hash === v2 ? v1.merge(v2, &merger) : v2}
+            self.merge!(other, &merger)
+          end
+        end
+
+        def get_roles(node_file)
+          roles = []
+          JSON.parse(File.read(node_file))['run_list'].each do |role|
+            roles << role.gsub(/role\[(.+)\]/, '\1') if /role\[(.+)\]/ === role
+          end
+          roles
+        end
+
+        def get_recipes(role)
+          recipes = []
+          JSON.parse(File.read("roles/#{role}.json"))['run_list'].each do |recipe|
+            if /recipe\[(.+)::(.+)\]/ === recipe
+              recipes << {recipe.gsub(/recipe\[(.+)::(.+)\]/, '\1') => recipe.gsub(/recipe\[(.+)::(.+)\]/, '\2')}
+            else
+              recipes << {recipe.gsub(/recipe\[(.+)\]/, '\1') => nil}
+            end
+          end
+          recipes
+        end
+
+        def get_node_recipes(node_file)
+          recipes = []
+          JSON.parse(File.read(node_file))['run_list'].each do |recipe|
+            if /recipe\[(.+)::(.+)\]/ === recipe
+              recipes << {recipe.gsub(/recipe\[(.+)::(.+)\]/, '\1') => recipe.gsub(/recipe\[(.+)::(.+)\]/, '\2')}
+            else
+              recipes << {recipe.gsub(/recipe\[(.+)\]/, '\1') => nil} unless /role\[(.+)\]/ === recipe
+            end
+          end
+          recipes
+        end
+
+        def jq(*objs)
+          par = nil
+          objs.each {|obj| par = JSON::pretty_generate(obj, :allow_nan => true, :max_nesting => false)}
+          return par
+        end
+
+        def write_json(filename)
+          File.open "tmp-nodes/#{filename}.json", 'w' do |f|
+            f.flock File::LOCK_EX
+            yield f
+            f.flock File::LOCK_UN
+          end
+        end
+
+        def hl
+          HighLine.new
+        end
+      end
+
+    end
+  end
+end
