e5c7ec91a8495eb79b570134a8ab7940f5232044
root <kamiyama@qualitia.co.jp>
2016-08-18 11:56:57 +0900

    fix _base logger

--- lib/itamae-mitsurin/mitsurin/creators/templates/project/site-cookbooks/_base/_base/recipes/default.rb
+++ lib/itamae-mitsurin/mitsurin/creators/templates/project/site-cookbooks/_base/_base/recipes/default.rb
@@ -1,1 +1,11 @@
-puts HighLine.new.color %! LOAD : Environment set is "#{node[:environments][:set]}"!, :green
+ItamaeMitsurin.logger.color(:green) do
+  ItamaeMitsurin.logger.info "Environment: #{node[:environments][:set]}"
+  ItamaeMitsurin.file_logger.info "Environment: #{node[:environments][:set]}"
+end
+
+#Aws.config.update({
+#  region: "ap-northeast-1",
+#  credentials: Aws::Credentials.new(
+#    'key',
+#    'secret'
+#)})
