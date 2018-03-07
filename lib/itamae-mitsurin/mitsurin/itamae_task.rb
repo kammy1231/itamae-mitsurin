5433be4b382c40fd8776f586f8feec4b047fe661
kammy <kammy@kammy-no-MacBook-Pro.local>
2018-03-07 18:10:07 +0900

    Add support for new base_task: itamae_task

--- lib/itamae-mitsurin/mitsurin/itamae_task.rb
+++ lib/itamae-mitsurin/mitsurin/itamae_task.rb
@@ -1,150 +1,64 @@
-require 'itamae-mitsurin/mitsurin/task_base'
-include Rake::DSL if defined? Rake::DSL
+require 'itamae-mitsurin/mitsurin/base_task'
 
 module ItamaeMitsurin
   module Mitsurin
-    class ItamaeTask
+    class ItamaeTask < BaseTask
+      ItamaeMitsurin.logger.formatter.colored = true
+      task = ItamaeTask.new
 
       namespace :itamae do
-        Dir.glob("nodes/**/*.json").each do |node_file|
-          bname = File.basename(node_file, '.json')
+        all = []
 
+        Dir.glob('nodes/**/*.json').each do |node_file|
           begin
-            node_h = JSON.parse(File.read(node_file), symbolize_names: true)
-          rescue JSON::ParserError => e
-            puts e.class.to_s + ", " + e.backtrace[0].to_s
-            puts "Node load error, nodefile:#{node_file}, reason:#{e.message}"
+            node_name = File.basename(node_file, '.json')
+            node = task.load_node_attributes(node_file)
+            node_short = node[:environments][:hostname].split('.')[0]
+          rescue => e
+            ItamaeMitsurin.logger.error e.inspect
+            ItamaeMitsurin.logger.info "From node file: #{node_file}"
+            exit 2
           end
 
-          desc "Itamae to #{bname}"
-          task node_h[:environments][:hostname].split(".")[0] do
+          all << node_short
+          desc 'Itamae to all nodes'
+          task 'all' => all
+
+          desc "Itamae to #{node_name}"
+          task node_short do
             begin
-              recipes = []
-              TaskBase.get_roles(node_file).each do |role|
-                recipes << TaskBase.get_recipes(role)
-              end
-              TaskBase.get_node_recipes(node_file).each do |recipe|
-                recipes << recipe
-              end
-            rescue Exception => e
-              puts e.class.to_s + ", " + e.backtrace[0].to_s
-              puts "Node or role error, nodefile:#{node_file}, reason:#{e.message}"
-            else
-              recipes.flatten!
-            end
+              run_list = task.load_run_list(node_file)
+              environments = task.load_environments(node)
+              recipe_attributes_list = task.load_recipe_attributes(run_list)
 
-            # get env attr
-            begin
-              env_set = node_h[:environments][:set]
-              raise "No environments set error" if env_set.nil?
-              env_h = JSON.parse(File.read("environments/#{env_set}.json"), symbolize_names: true)
-            rescue Exception => e
-              puts e.class.to_s + ", " + e.backtrace[0].to_s
-              puts "Node or environment load error, nodefile:#{node_file}, reason:#{e.message}"
-            end
+              merged_recipe = task.merge_attributes(recipe_attributes_list)
+              merged_environments = task.merge_attributes(merged_recipe, environments)
+              attributes = task.merge_attributes(merged_environments, node)
+              task.create_tmp_nodes(node_name, attributes)
 
-            # get recipes attr
-            recipe_attr_file = []
-            recipes.each do |recipe_h|
-              if recipe_h["#{recipe_h.keys.join}"] == "default"
-                recipe_attr_file.insert 0,
-                    Dir.glob("site-cookbooks/**/#{recipe_h.keys.join}/attributes/#{recipe_h["#{recipe_h.keys.join}"]}.json")
-              else
-                recipe_attr_file <<
-                    Dir.glob("site-cookbooks/**/#{recipe_h.keys.join}/attributes/#{recipe_h["#{recipe_h.keys.join}"]}.json")
-              end
-            end
+              command = task.create_itamae_command(node_name, attributes)
+              command_recipe = task.list_recipe_filepath(run_list)
+              command_recipe.sort_by! {|item| File.dirname(item) }
+              command << command_recipe.join
 
-            recipe_attr_file.flatten!
-
-            # recipes attr other=env
-            recipe_env_h_a = []
-            recipe_attr_file.each do |file|
-              recipe_h = JSON.parse(File.read(file), symbolize_names: true)
-              recipe_env_h_a << recipe_h.deep_merge(env_h)
-            end
-
-            # recipe attr other=recipes_env
-            moto = recipe_env_h_a[0]
-            recipe_env_h_a.each {|hash| moto.deep_merge!(hash)}
-            recipe_env_h = moto
-
-            if recipe_env_h.nil?
-              # env attr other=node
-              node_env_h = env_h.deep_merge(node_h)
-              node_env_j = TaskBase.jq node_env_h
-              TaskBase.write_json(bname) {|file| file.puts node_env_j}
-            else
-              # recipe_env attr other=node
-              recipe_env_node_h = recipe_env_h.deep_merge(node_h)
-              recipe_env_node_j = TaskBase.jq recipe_env_node_h
-              TaskBase.write_json(bname) {|file| file.puts recipe_env_node_j}
-            end
-
-            recipes << {'_base' => 'default'}
-            node_property = JSON.parse(File.read("tmp-nodes/#{bname}.json"), symbolize_names: true)
-            node = node_property[:environments][:hostname]
-            ssh_user = node_property[:environments][:ssh_user]
-            ssh_password = node_property[:environments][:ssh_password]
-            sudo_password = node_property[:environments][:sudo_password]
-            ssh_port = node_property[:environments][:ssh_port]
-            ssh_key = node_property[:environments][:ssh_key]
-
-            ENV['TARGET_HOST'] = node
-            ENV['NODE_FILE'] = node_file
-            ENV['SSH_PASSWORD'] = ssh_password
-            ENV['SUDO_PASSWORD'] = sudo_password
-
-            command = "bundle exec itamae ssh"
-            command << " -h #{node}"
-            command << " -u #{ssh_user}"
-            command << " -p #{ssh_port}"
-            command << " -i keys/#{ssh_key}" unless ssh_key.nil?
-            command << " -j tmp-nodes/#{bname}.json"
-            command << " --shell=bash"
-            command << " --ask-password" unless ssh_password.nil?
-            command << " --dry-run" if ENV['dry-run'] == "true"
-            command << " -l debug" if ENV['debug'] == "true"
-            command << " -c logs/config/itamae_task.config"
-
-            # Pass to read the recipe command
-            command_recipe = []
-            recipes.each do |recipe_h|
-              target_recipe = "site-cookbooks/**/#{recipe_h.keys.join}/recipes/#{recipe_h[recipe_h.keys.join]}.rb"
-              if Dir.glob(target_recipe).empty?
-                raise "Recipe load error, nodefile: #{node_file}, reason: Does not exist " +
-                      recipe_h.keys.join + '::' +recipe_h.values.join
-              end
-              Dir.glob(target_recipe).join("\s").split.each do |target|
-                unless File.exists?(target)
-                  ex_recipe = recipe_h.to_s.gsub('=>', '::').gsub('"', '')
-                  raise "Recipe load error, nodefile:#{node_file}, reason: Does not exist #{ex_recipe}"
+              task.runner_display(attributes[:run_list], run_list, command)
+              st = system command
+              if st
+                ItamaeMitsurin.logger.color(:green) do
+                  ItamaeMitsurin.logger.info 'itamae_task is completed.'
                 end
-                command_recipe << " #{target}"
-              end
-            end
-
-            command_recipe.sort_by! {|item| File.dirname(item) }
-            command << command_recipe.join
-
-            puts TaskBase.hl.color(%!Run Itamae to "#{bname}"!, :red)
-            run_list_noti = []
-            command_recipe.each { |c_recipe|
-              unless c_recipe.split("/")[4].split(".")[0] == 'default'
-                run_list_noti << c_recipe.split("/")[2] + "::#{c_recipe.split("/")[4].split(".")[0]}"
               else
-                run_list_noti << c_recipe.split("/")[2]
+                ItamaeMitsurin.logger.error 'itamae_task is failed.'
+                exit 1
               end
-            }
-
-            puts TaskBase.hl.color(%!Run List to \"#{run_list_noti.uniq.join(", ")}\"!, :green)
-            puts TaskBase.hl.color(%!#{command}!, :white)
-            st = system command
-            exit 1 unless st
+            rescue => e
+              ItamaeMitsurin.logger.error e.inspect
+              ItamaeMitsurin.logger.info "From node file: #{node_file}"
+              exit 2
+            end
           end
         end
       end
-
     end
   end
 end
